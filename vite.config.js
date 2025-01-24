6a561499eaf3efbfa989df8ff87877a645e4251d
Nahuel-Beck <juannahuelbeck@gmail.com>
2025-01-24 11:57:29 -0300

    Configuraciones de parámetros y rutas de archivos

--- vite.config.js
+++ vite.config.js
@@ -12,10 +12,10 @@ export default defineConfig({
         nosotros: path.resolve(__dirname, 'nosotros.html'),
         obrasedes: path.resolve(__dirname, 'obras-y-sedes.html'),
       },
-      external: ['@fortawesome/fontawesome-svg-core'],
+      // external: ['@fortawesome/fontawesome-svg-core'],
     },
   },
   optimizeDeps: {
-    include: ['file-upload-with-preview'],
+    include: ['gsap','file-upload-with-preview','@fortawesome/fontawesome-svg-core'],
   },
 });
